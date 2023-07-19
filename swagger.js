585467ce26810bd7282a36cb82a9a3753d4940a0
Billy Estella <billyestella02@gmail.com>
2023-07-20 02:32:44 +0800

    udpated render url

--- swagger.js
+++ swagger.js
@@ -3,9 +3,9 @@ import swaggerAutogen from 'swagger-autogen';
 const doc = {
     info: {
         title: 'CSE341 Final Project API',
-        description: 'A simple API for a basic E-comerce web app'
+        description: 'A simple API for a basic E-commerce web app'
     },
-    host: 'cse341-final-project.onrender.com',
+    host: 'cse341-final-project-de7s.onrender.com',
     schemes: ['https']
 };
 
